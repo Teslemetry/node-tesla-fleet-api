0e3bffdb5c0745b45fd6068f152c319a274aebc5
Brett Adams <hi@ba.au>
2026-08-02 21:57:42 +1000

    Remove vendored protobuf sources; @teslemetry/tesla-protocol is sole source
    The library already imported all protobuf types from @teslemetry/tesla-protocol;
    protobuf/*.proto and createPb2.sh were leftover, unreferenced vendored copies.

--- createPb2.sh
+++
@@ -1,7 +0,0 @@
-protoc \
-    --plugin="protoc-gen-ts=./node_modules/.bin/protoc-gen-ts" \
-    --js_out="import_style=commonjs,binary:src/pb2" \
-    --ts_out="src/pb2" \
-    --ts_opt="esModuleInterop=true" \
-    -I=protobuf \
-    protobuf/*.proto
